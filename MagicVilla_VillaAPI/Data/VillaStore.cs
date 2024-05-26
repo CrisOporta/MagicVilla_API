7b211d8685f526a163b3b51880cf1d4c7fed837e
crisoporta2002@gmail.com <crisoporta2002@gmail.com>
2024-05-25 19:54:12 -0600

    Section 4: Dependency Injection and DBContext

---
+++ MagicVilla_VillaAPI/Data/VillaStore.cs
@@ -0,0 +1,13 @@
+﻿using MagicVilla_VillaAPI.Models.Dto;
+
+namespace MagicVilla_VillaAPI.Data
+{
+    public static class VillaStore
+    {
+        public static List<VillaDTO> villaList = new List<VillaDTO>
+            {
+                new VillaDTO { Id = 1, Name = "Pool View", Sqfc = 100, Ocuppancy = 4}, 
+                new VillaDTO { Id = 2, Name = "Beach View", Sqfc = 300, Ocuppancy = 3}
+            };
+    }
+}
